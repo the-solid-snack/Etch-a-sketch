ef7397d7c1ebe780b165e61c7d2168a39ec93b2b
Greyfox <marco.andolfatto.uha@gmail.com>
2020-04-14 19:05:27 +0100

    commit de branche!

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const wrapper = document.querySelector(".wrapper");
-let gridSize = 16;
+let gridSize = 256;
+let root = document.documentElement;
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.className = "pixel";
-  wrapper.appendChild(div);
-}
-
-const allPixels = document.querySelectorAll(".pixel");
-
-allPixels.forEach((div) => {
-  div.addEventListener("mouseover", (e) => {
-    div.classList.add("active");
+function createGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    div.className = "pixel";
+    wrapper.appendChild(div);
+  }
+  const allPixels = document.querySelectorAll(".pixel");
+  allPixels.forEach((div) => {
+    div.addEventListener("mouseover", (e) => {
+      div.classList.add("active");
+    });
   });
-});
+}
 
 const reset = document.querySelector("#reset");
 reset.addEventListener("click", () => {
+  const allPixels = document.querySelectorAll(".pixel");
   allPixels.forEach((div) => {
       div.classList.remove("active");
   });
+});
+
+const start = document.querySelector("#start");
+start.addEventListener("click", () => {
+  createGrid();
+});
+
+const threeTwo = document.querySelector("#32");
+plus.addEventListener("click", () => {
+  
 });

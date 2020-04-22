dc32c3612fc3eb57007935e65da48900928a3658
Greyfox <marco.andolfatto.uha@gmail.com>
2020-04-22 13:18:46 +0100

    co-co-co-COMMIT

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const wrapper = document.querySelector(".wrapper");
 let gridSize=256;
 let root = document.documentElement;
+const reset = document.querySelector("#reset");
+const start = document.querySelector("#start");
+const plus = document.querySelector("#plus");
 
 function createGrid(gridSize) {
   let gridArea = gridSize;
@@ -18,7 +21,7 @@ function createGrid(gridSize) {
 }
 
 function resetGrid() {
-  const reset = document.querySelector("#reset");
+  //const reset = document.querySelector("#reset");
   reset.addEventListener("click", () => { 
     const allPixels = document.querySelectorAll(".pixel");
     allPixels.forEach((div) => {
@@ -27,17 +30,17 @@ function resetGrid() {
   });
 }
 
-const start = document.querySelector("#start");
+
 start.addEventListener("click", () => {
-  createGrid();
+  createGrid(256);
 });
 
-const reset = document.querySelector("#reset");
+
 reset.addEventListener("click", () => {
   resetGrid();
 });
 
-const plus = document.querySelector("#plus");
+
 plus.addEventListener ("click", () => {
   root.style.setProperty("--rowNum", 32);
   root.style.setProperty("--colNum", 32);
